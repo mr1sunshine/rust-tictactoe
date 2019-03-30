29d4a6dd4c46c940cd8bd3d3307594fd20edf94f
Alex <Alexander.Ovchinnikof@gmail.com>
2019-03-30 11:50:50 +0200

    Moved draw functions to separate file.

--- src/ttt/game_ui.rs
+++ src/ttt/game_ui.rs
@@ -1,13 +1,13 @@
+use super::draw_helpers::*;
+
 use ggez::{
     event::{self, MouseButton},
-    graphics::{self, DrawParam, MeshBuilder, Text},
-    nalgebra::Point2,
+    graphics::{self, DrawParam, MeshBuilder},
     Context, GameResult,
 };
 
-use super::game::{
-    Cell, FieldType, Game, GameState, Player, PLAY_FIELD_POS, PLAY_FIELD_SIZE, SQUARE_SIZE,
-};
+use super::game::{Cell, FieldType, Game, GameState, Player, PLAY_FIELD_SIZE};
+
 use super::game_logic;
 
 impl event::EventHandler for Game {
@@ -64,114 +64,4 @@ impl event::EventHandler for Game {
         ggez::timer::yield_now();
         Ok(())
     }
-}
-
-fn draw_field(mb: &mut MeshBuilder) {
-    for i in 0..PLAY_FIELD_SIZE + 1 {
-        let _ = mb.line(
-            &[
-                Point2::new(PLAY_FIELD_POS.0 + SQUARE_SIZE * i as f32, PLAY_FIELD_POS.1),
-                Point2::new(
-                    PLAY_FIELD_POS.0 + SQUARE_SIZE * i as f32,
-                    PLAY_FIELD_POS.1 + SQUARE_SIZE * PLAY_FIELD_SIZE as f32,
-                ),
-            ],
-            4.0,
-            graphics::BLACK,
-        );
-    }
-    for i in 0..PLAY_FIELD_SIZE + 1 {
-        let _ = mb.line(
-            &[
-                Point2::new(PLAY_FIELD_POS.0, PLAY_FIELD_POS.1 + SQUARE_SIZE * i as f32),
-                Point2::new(
-                    PLAY_FIELD_POS.0 + SQUARE_SIZE * PLAY_FIELD_SIZE as f32,
-                    PLAY_FIELD_POS.1 + SQUARE_SIZE * i as f32,
-                ),
-            ],
-            4.0,
-            graphics::BLACK,
-        );
-    }
-}
-
-fn draw_red_line(mb: &mut MeshBuilder, index_first: usize, index_second: usize) {
-    let (point1_y, point1_x) = (index_first / 3, index_first % 3);
-    let (point2_y, point2_x) = (index_second / 3, index_second % 3);
-    let red_color = graphics::Color::from_rgb_u32(0x00FF0000);
-    let _ = mb.line(
-        &[
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * point1_x as f32 + SQUARE_SIZE / 2.0,
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * point1_y as f32 + SQUARE_SIZE / 2.0,
-            ),
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * point2_x as f32 + SQUARE_SIZE / 2.0,
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * point2_y as f32 + SQUARE_SIZE / 2.0,
-            ),
-        ],
-        10.0,
-        red_color,
-    );
-}
-
-fn draw_player1(mb: &mut MeshBuilder, pos_x: usize, pos_y: usize) {
-    mb.circle(
-        graphics::DrawMode::stroke(4.0),
-        Point2::new(
-            PLAY_FIELD_POS.0 + (pos_x as f32 + 0.5) * SQUARE_SIZE,
-            PLAY_FIELD_POS.1 + (pos_y as f32 + 0.5) * SQUARE_SIZE,
-        ),
-        SQUARE_SIZE as f32 / 4.0,
-        0.00001,
-        graphics::BLACK,
-    );
-}
-
-fn draw_player2(mb: &mut MeshBuilder, pos_x: usize, pos_y: usize) {
-    let _ = mb.line(
-        &[
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * (pos_x as f32 + 0.25),
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * (pos_y as f32 + 0.25),
-            ),
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * (pos_x as f32 + 0.75),
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * (pos_y as f32 + 0.75),
-            ),
-        ],
-        4.0,
-        graphics::BLACK,
-    );
-    let _ = mb.line(
-        &[
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * (pos_x as f32 + 0.75),
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * (pos_y as f32 + 0.25),
-            ),
-            Point2::new(
-                PLAY_FIELD_POS.0 + SQUARE_SIZE * (pos_x as f32 + 0.25),
-                PLAY_FIELD_POS.1 + SQUARE_SIZE * (pos_y as f32 + 0.75),
-            ),
-        ],
-        4.0,
-        graphics::BLACK,
-    );
-}
-
-fn draw_player(mb: &mut MeshBuilder, player: Player, pos_x: usize, pos_y: usize) {
-    match player {
-        Player::Player1 => draw_player1(mb, pos_x, pos_y),
-        Player::Player2 => draw_player2(mb, pos_x, pos_y),
-    }
-}
-
-fn draw_text(ctx: &mut Context) {
-    let fps_display = Text::new(format!("FPS: test"));
-    // When drawing through these calls, `DrawParam` will work as they are documented.
-    let _ = graphics::draw(
-        ctx,
-        &fps_display,
-        (Point2::new(200.0, 0.0), graphics::BLACK),
-    );
 }
--- src/ttt/mod.rs
+++ src/ttt/mod.rs
@@ -1,3 +1,4 @@
 pub mod game;
 pub mod game_logic;
 pub mod game_ui;
+mod draw_helpers;
