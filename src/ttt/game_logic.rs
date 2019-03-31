0a94ebba0c34c8688053d5853241ccdb2d84379b
Alex <Alexander.Ovchinnikof@gmail.com>
2019-03-31 21:24:00 +0300

    Added alpha beta pruning algorithm.

--- src/ttt/game_logic.rs
+++ src/ttt/game_logic.rs
@@ -1,6 +1,6 @@
 use super::game::{Cell, Game, GameState, Player};
 
-fn max_search(elements: &mut [Cell]) -> i32 {
+fn max_search(elements: &mut [Cell], mut alpha: i32, beta: i32) -> i32 {
     let game_state = Game::get_game_state(elements);
     match game_state {
         GameState::GameWon { player, .. } => {
@@ -9,20 +9,24 @@ fn max_search(elements: &mut [Cell]) -> i32 {
         GameState::Tie => return 0,
         GameState::InProgress => (),
     }
-
-    let mut score = std::i32::MIN;
 
     for i in 0..elements.len() {
         if elements[i] == Cell::Empty {
             elements[i] = Cell::Player(Player::Player1);
-            score = std::cmp::max(score, min_search(elements));
+            let score = min_search(elements, alpha, beta);
             elements[i] = Cell::Empty;
+            if score > alpha {
+                alpha = score;
+            }
+            if alpha > beta {
+                break;
+            }
         }
     }
-    score
+    alpha
 }
 
-fn min_search(elements: &mut [Cell]) -> i32 {
+fn min_search(elements: &mut [Cell], alpha: i32, mut beta: i32) -> i32 {
     let game_state = Game::get_game_state(elements);
     match game_state {
         GameState::GameWon { player, .. } => {
@@ -32,16 +36,20 @@ fn min_search(elements: &mut [Cell]) -> i32 {
         GameState::InProgress => (),
     }
 
-    let mut score = std::i32::MAX;
-
     for i in 0..elements.len() {
         if elements[i] == Cell::Empty {
             elements[i] = Cell::Player(Player::Player2);
-            score = std::cmp::min(score, max_search(elements));
+            let score = max_search(elements, alpha, beta);
             elements[i] = Cell::Empty;
+            if score < beta {
+                beta = score;
+            }
+            if alpha > beta {
+                break;
+            }
         }
     }
-    score
+    beta
 }
 
 pub fn get_best_move(elements: &mut [Cell]) -> Option<usize> {
@@ -52,7 +60,7 @@ pub fn get_best_move(elements: &mut [Cell]) -> Option<usize> {
     for i in 0..elements.len() {
         if elements[i] == Cell::Empty {
             elements[i] = Cell::Player(Player::Player1);
-            let tmp_score = min_search(elements);
+            let tmp_score = min_search(elements, std::i32::MIN, std::i32::MAX);
             elements[i] = Cell::Empty;
 
             if tmp_score > best_score {
